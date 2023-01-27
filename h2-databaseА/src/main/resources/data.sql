3406715cbdba1340742ebe326829a93cbb193312
Anton Almishev <119037539+AntonAlmishev@users.noreply.github.com>
2023-01-27 09:19:59 +0200

    Add files via upload

---
+++ h2-databaseА/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('Любо', 'Алмишев', 'uting@gmail.com'),
+  	('Иван', 'Алмишев', 'h2db@gmail.com'),
+  	('Антонис', 'Алмишев', 'inmemory@gmail.com');
+  	
